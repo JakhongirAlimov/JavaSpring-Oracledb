ef8e2002a13de2f3054883c76e9362a01b97931e
Jahongir Alimov (Ziraatbank UZ) <jalimov@utbank.local>
2022-07-21 09:57:02 +0500

    ADD DATABASE MYSQL

--- src/main/java/com/itproger/blog/controllers/MainController.java
+++ src/main/java/com/itproger/blog/controllers/MainController.java
@@ -9,8 +9,13 @@ public class MainController {
 
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("title", "главная страница");
+        model.addAttribute("title", "Главная страница");
         return "home";
+    }
+    @GetMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("title", "Страница про нас");
+        return "about";
     }
 
 }
