9d431aee4cb5fd75b563f0570d63fc026fa7e53f
Jahongir Alimov (Ziraatbank UZ) <jalimov@utbank.local>
2022-08-03 10:42:10 +0500

    my changes

--- src/main/java/com/itproger/blog/models/Post.java
+++ src/main/java/com/itproger/blog/models/Post.java
@@ -31,6 +31,7 @@ public class Post {
         this.title = title;
     }
 
+
     public String getAnons() {
         return anons;
     }
@@ -54,4 +55,12 @@ public class Post {
     public void setViews(int views) {
         this.views = views;
     }
+
+    public  Post(){}
+    public Post(String title, String anons, String full_text) {
+        this.title = title;
+        this.anons = anons;
+        this.full_text = full_text;
+    }
+
 }
